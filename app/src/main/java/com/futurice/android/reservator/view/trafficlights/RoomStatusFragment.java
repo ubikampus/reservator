089043e72d23ab94cbad8ffdb2327c81cd03b83a
Brandon <brandon.startz@gmail.com>
2018-10-31 15:36:20 +0200

    UI changes - progress bar - disconnected
    Flipped progress bar on X axis so it fills rather than empties.
    
    Changed color of progress bar - however since it is flipped on the X the background color becomes the fill color, which is a bit faint.
    
    I added all of the code for the disconnected symbol as well as making the main text invisible while disconnected is displayed, but in the old method it was called from init and update, and we have neither in that fragment. I am unsure of where to put them.
    
    Further, the main text of the screen becomes invisible when Disconnected activates, except use room now is called by TrafficLightsPresenter which overrides what I am trying to do. I think I have a solution because perhaps someone else would like to implement something cleaner than my hack and slash approach

--- app/src/main/java/com/futurice/android/reservator/view/trafficlights/RoomStatusFragment.java
+++ app/src/main/java/com/futurice/android/reservator/view/trafficlights/RoomStatusFragment.java
@@ -1,5 +1,7 @@
 package com.futurice.android.reservator.view.trafficlights;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,10 +11,19 @@ import android.widget.TextView;
 
 import com.futurice.android.reservator.R;
 
+import java.util.Date;
+
+import butterknife.BindView;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
 public class RoomStatusFragment extends Fragment {
+    private static Date lastTimeConnected = new Date(0);
+    private final long DISCONNECTED_WARNING_ICON_THRESHOLD = 5 * 60 * 1000;
 
     public interface RoomStatusPresenter {
-    void setRoomStatusFragment(RoomStatusFragment fragment);
+        void setRoomStatusFragment(RoomStatusFragment fragment);
     }
 
     private RoomStatusPresenter presenter;
@@ -23,10 +34,13 @@ public class RoomStatusFragment extends Fragment {
     private TextView meetingNameText = null;
     private TextView bookNowText = null;
 
+    @BindView(R.id.disconnected)
+    View disconnected;
+
     public void setPresenter(RoomStatusPresenter presenter) {
         this.presenter = presenter;
         this.presenter.setRoomStatusFragment(this);
-        }
+    }
 
     @Override
     public void onAttach(Context context) {
@@ -51,6 +65,52 @@ public class RoomStatusFragment extends Fragment {
         return view;
     }
 
+    private void updateConnected() {
+        ConnectivityManager cm = null;
+        try {
+            cm = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        } catch (ClassCastException cce) {
+            return;
+        }
+        if (cm == null) return;
+
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+
+        if (ni != null && ni.isConnectedOrConnecting()) {
+            // Connected
+            lastTimeConnected = new Date();
+            if (disconnected.getVisibility() != GONE) {
+                disconnected.setVisibility(GONE);
+            }
+            if(this.statusText.getVisibility() != VISIBLE){
+                statusText.setVisibility(VISIBLE);
+            }
+            if(this.statusUntilText.getVisibility() != VISIBLE){
+                statusUntilText.setVisibility(VISIBLE);
+            }
+            if(this.meetingNameText.getVisibility() != VISIBLE){
+                meetingNameText.setVisibility(VISIBLE);
+            }
+
+        } else {
+            // Disconnected
+            if (lastTimeConnected.before(new Date(new Date().getTime() - DISCONNECTED_WARNING_ICON_THRESHOLD))) {
+                if (disconnected.getVisibility() != VISIBLE) {
+                    disconnected.setVisibility(VISIBLE);
+                }
+                if(this.statusText.getVisibility() != GONE){
+                    statusText.setVisibility(GONE);
+                }
+                if(this.statusUntilText.getVisibility() != GONE){
+                    statusUntilText.setVisibility(GONE);
+                }
+                if(this.meetingNameText.getVisibility() != GONE){
+                    meetingNameText.setVisibility(GONE);
+                }
+            }
+        }
+    }
+
     public void setRoomTitleText(String text) {
         this.roomTitleText.setText(text);
     }
@@ -64,9 +124,10 @@ public class RoomStatusFragment extends Fragment {
         this.meetingNameText.setText(text);
     }
     public void showBookNowText() {
-        this.bookNowText.setVisibility(View.VISIBLE);
+        this.bookNowText.setVisibility(VISIBLE);
     }
     public void hideBookNowText() {
-        this.bookNowText.setVisibility(View.GONE);
+        this.bookNowText.setVisibility(GONE);
     }
 }
+
