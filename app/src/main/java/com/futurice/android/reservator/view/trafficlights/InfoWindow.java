fcbfe9004a8f4c8e0c75a69de1a36e41efb4561b
Vera Izrailit <vera.izrailit@futurice.com>
2018-12-10 16:10:43 +0200

    Visial changes to info window.

--- app/src/main/java/com/futurice/android/reservator/view/trafficlights/InfoWindow.java
+++ app/src/main/java/com/futurice/android/reservator/view/trafficlights/InfoWindow.java
@@ -1,5 +1,6 @@
 package com.futurice.android.reservator.view.trafficlights;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
@@ -7,12 +8,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import android.widget.TextView;
+import com.futurice.android.reservator.BuildConfig;
 import com.futurice.android.reservator.R;
 
 
 public class InfoWindow extends DialogFragment {
 
     Button okButton;
+    TextView versionName;
 
     private void onOkClicked() {
         this.dismissAllowingStateLoss();
@@ -24,6 +28,8 @@ public class InfoWindow extends DialogFragment {
 		View view = inflater.inflate(R.layout.info_window, container);
 
         this.okButton = (Button) view.findViewById(R.id.infoOkButton);
+        this.versionName = view.findViewById(R.id.infoAppVersion);
+        versionName.setText(getString(R.string.version, BuildConfig.VERSION_NAME));
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
